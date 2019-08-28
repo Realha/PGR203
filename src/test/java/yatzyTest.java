9f56a72e1b6c1fb0f5e8407572ecf995d19a6870
Remi Havn <remihavn@gmail.com>
2019-08-28 16:48:58 +0200

    creating git

---
+++ src/test/java/yatzyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class yatzyTest {
+    @Test
+    void checkIfFour() {
+        assertEquals(4, 2+2);
+    }
+}
