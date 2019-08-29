1dca4803a4fca95ac506a1ecc44ff39663ef8ef1
Remi Havn <remihavn@gmail.com>
2019-08-29 12:25:25 +0200

    Added score method

--- src/test/java/yatzyTest.java
+++ src/test/java/yatzyTest.java
@@ -4,7 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class yatzyTest {
     @Test
-    void checkIfFour() {
-        assertEquals(4, 2+2);
+    void checkIfFour() { assertEquals(4, 2+2);}
+
+    @Test
+    void shouldCalculateForOnes() {
+        assertEquals(0, score("ONES", new int[] { 2, 3, 4, 5, 6 }));
+        assertEquals(3, score("ONES", new int[] { 2, 1, 4, 1, 1 }));
+        assertEquals(5, score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+    }
+
+    private int score(String ones, int[] ints) {
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 1) {
+                sum += 1;
+            }
+        }
+        return sum;
     }
 }
