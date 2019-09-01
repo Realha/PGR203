13a52e1b989d1cfc1486be17effb1438a52d413d
gulpipose <guro.olsen@gmail.com>
2019-09-01 22:05:00 +0200

    adds threes test

--- src/test/java/yatzyTest.java
+++ src/test/java/yatzyTest.java
@@ -4,29 +4,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class yatzyTest {
     @Test
-    void checkIfFour() { assertEquals(4, 2+2);}
+    void checkIfFour() {
+        assertEquals(4, 2 + 2);
+    }
 
     @Test
     void shouldCalculateForOnes() {
-        assertEquals(0, score("ONES", new int[] { 2, 3, 4, 5, 6 }));
-        assertEquals(3, score("ONES", new int[] { 2, 1, 4, 1, 1 }));
-        assertEquals(5, score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+        assertEquals(0, score("ONES", new int[]{2, 3, 4, 5, 6}));
+        assertEquals(3, score("ONES", new int[]{2, 1, 4, 1, 1}));
+        assertEquals(5, score("ONES", new int[]{1, 1, 1, 1, 1}));
     }
 
     @Test
     void shouldCalculateForTwos() {
-        assertEquals(0, score("TWOS", new int[] { 1, 3, 4, 5, 6}));
-        assertEquals(2, score("TWOS", new int[] { 2, 3, 2, 5, 6}));
-        assertEquals(5, score("TWOS", new int[] { 2, 2, 2, 2, 2}));
+        assertEquals(0, score("TWOS", new int[]{1, 3, 4, 5, 6}));
+        assertEquals(4, score("TWOS", new int[]{2, 3, 2, 5, 6}));
+        assertEquals(10, score("TWOS", new int[]{2, 2, 2, 2, 2}));
     }
 
-    private int score(String ones, int[] ints) {
-        int sum = 0;
-        for (int i = 0; i < ints.length; i++) {
-            if (ints[i] == 1) {
-                sum += 1;
+    @Test
+    void shouldCalculateForThrees(){
+        assertEquals(3, score("THREES", new int[]{5, 6, 3, 2, 1}));
+        assertEquals(9, score("THREES", new int[]{3, 3, 6, 3, 5}));
+        assertEquals(6, score("THREES", new int[]{3, 6, 2, 4, 3}));
+    }
+
+    private int score(String desiredNum, int[] diceRolls) {
+        int score = 0;
+
+        for (int diceRoll : diceRolls) {
+            switch (desiredNum) {
+                case "ONES":
+                    if (diceRoll == 1) {
+                        score += 1;
+                    }
+                    break;
+                case "TWOS":
+                    if (diceRoll == 2)
+                        score += 2;
             }
         }
-        return sum;
+        return score;
     }
 }
